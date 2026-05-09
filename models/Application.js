10636968a08954ed21759b4eb7324d0e422baa6c
Alexandra White <awhite31@terpmail.umd.edu>
2026-05-08 22:23:41 -0400

    what each file is for

---
+++ models/Application.js
@@ -0,0 +1,8 @@
+/* Defines the MongoDB schema using Mongoose
+Responsibilites:
+- Creates the structure for the internship applications
+- Stores: company name, role title, application status, notes
+            logo URL, dates, links
+
+    
+*/ 
--- routes/applications.js
+++ routes/applications.js
@@ -1,3 +1,18 @@
+/* Handles all application-related routes 
+Responibilies:  
+    - displays add form
+    - save applications to MongoDB
+    - retrieve all applications
+    - retrieve one application
+    - delete/edit application later
+
+EXAMPLE Routes 
+GET /applications
+GET /applications/add
+POST /applications/add
+GET /applications/:id
+*/
+
 const express = require("express");
 
 const router = express.Router();
